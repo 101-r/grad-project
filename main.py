58c617ebc0f199528e2df5fb75964eef410f8be5
101-r <ravshan.cau@gmail.com>
2024-12-11 10:10:50 +0500

    chore: modify main.py to test workflow functionality

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from utils.extractor import extractor
 from src.ollama import text_model, embeddings
 
 async def main():
-    text = extractor.extract_pdf(path="/home/fedora-1/Downloads/grokking-ml.pdf")
+    text = extractor.extract_pdf(path="/home/fedora-r/Downloads/grokking-deep-learning.pdf", max_pages=10)
 
     summary = await text_model.summary(text)
 
+    keywords = extractor.extract_keywords(summary)
+
     embedding = await embeddings.text(summary)
 
-    milvus.insert("/home/fedora-1/Downloads/grokking-ml.pdf", embedding)
-    
-    query_embedding = await embeddings.text("")
+    milvus.insert("/home/fedora-r/Downloads/grokking-deep-learning.pdf", embedding, keywords)
+
+    query_embedding = await embeddings.text("AI")
 
     print(milvus.search(query_embedding))
 
